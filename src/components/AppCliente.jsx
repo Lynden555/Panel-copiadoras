2b382302606963688c8a6ed8d6d0aca3b3eef4fe
Lynden555 <diegollera1@gmail.com>
2025-08-01 15:03:22 -0700

    Actualizacion Apk

--- src/components/AppCliente.jsx
+++ src/components/AppCliente.jsx
@@ -19,7 +19,7 @@ const AppCliente = () => {
       </div>
 
       <a 
-        href="/GrapeCliente.apk" 
+        href="/GrapeClientes.apk" 
         download 
         className="boton-descarga"
       >
