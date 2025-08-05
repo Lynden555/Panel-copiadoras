c67557e6dd75379f4410432e97ae9ad27b0e6137
Lynden555 <diegollera1@gmail.com>
2025-08-05 13:53:28 -0700

    Actualizacion APK 8

--- src/components/AppTecnico.jsx
+++ src/components/AppTecnico.jsx
@@ -18,7 +18,7 @@ const AppTecnico = () => {
       </div>
 
       <a 
-        href="/Tecnicos-Grape.apk" // 👈 Reemplaza con tu ruta final tipo "/GrapeTecnico.apk"
+        href="/GrapeTec.apk" // 👈 Reemplaza con tu ruta final tipo "/GrapeTecnico.apk"
         download 
         className="boton-descarga"
       >
