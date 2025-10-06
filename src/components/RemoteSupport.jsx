c5c2766df1b04ab841fe3a068ef34e0408260bc4
Lynden555 <diegollera1@gmail.com>
2025-10-06 16:13:01 -0700

    Actualizacion Remoto 3

--- src/components/RemoteSupport.jsx
+++ src/components/RemoteSupport.jsx
@@ -14,7 +14,7 @@ import KeyboardIcon from "@mui/icons-material/Keyboard";
 import CloseIcon from "@mui/icons-material/Close";
 
 const API_BASE = "https://copias-backend-production.up.railway.app";
-const SIGNALING_URL = "wss://grapeassist.org/signal";
+const SIGNALING_URL = "wss://grapeassist.org/signal/";
 
 const RTC_CONFIG = {
   iceServers: [
